5cb411e8cbdfead47b747938328606c99a11a340
Gary Carr <Gary.Carr@americanfidelity.com>
2019-02-22 11:02:48 -0600

    Adding new SQL managed instance script to use for creating VM for SQLMI

---
+++ go.ps1
@@ -0,0 +1,12 @@
+$scriptUrlBase = 'https://github.com/garycarrAF/arm-templates'
+
+$parameters = @{
+    subscriptionId = 'ebbdd8a0-9d65-43d0-9526-73ec87632296'
+    resourceGroupName = 'af-core-net-rg'
+    virtualMachineName = 'mazd-dbmi01'
+    virtualNetworkName = 'microsegmentation-vnet'
+    subnetName = 'data-paas'
+    administratorLogin  = 'adminuser'
+    administratorLoginPassword  = '!Qazxdr5!Qazxdr5'
+}
+Invoke-Command -ScriptBlock ([Scriptblock]::Create((iwr ($scriptUrlBase+'/sql-mi.ps1?t='+ [DateTime]::Now.Ticks)).Content)) -ArgumentList $parameters, $scriptUrlBase
